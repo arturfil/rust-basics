88820dd176a7b1fb8fc9c88227b1e95b94fdffc2
arturfil <arturfil@hotmail.com>
2023-04-05 10:10:49 -0400

    added more collections:

---
+++ src/collections/strings.rs
@@ -0,0 +1,13 @@
+
+
+pub fn strings_main() {
+    let s = String::new();
+    let data = "initial contents";
+    let s = data.to_string();
+    let s = "initial contents";
+    let t = String::from("initial content");
+    
+
+    let mut k = String::from("foo");
+    k.push_str("bar");
+}
